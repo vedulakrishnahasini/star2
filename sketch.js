b00a6c23bcd6ace1518356e535fe80ca0242f850
vedulakrishnahasini <76158584+vedulakrishnahasini@users.noreply.github.com>
2021-02-09 20:08:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	// fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	var render = Render.create({
+		element: document.body,
+		engine: engine,
+		options:{
+			width:1200,
+			height:700,
+			wireframes:false
+		}
+	});
+	Engine.run(engine);
+	Render.run(render);
+	
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  drawSprites();
+  star.x=starBody.position.x;
+  star.y=starBody.position.y;
+
+}
+
+function keyPressed() {
+	//write code here
+	if(keyCode=== LEFT_ARROW){
+		fairy.x=fairy.x-10;
+	}
+
+	if(keyCode===RIGHT_ARROW){
+		fairy.x=fairy.x+10;
+	}
+
+	if(keyCode===DOWN_ARROW){
+		Body.setStatic(starBody,false)
+	}
+}
